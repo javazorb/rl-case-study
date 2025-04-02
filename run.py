42b582313a6209511b15c25106e142b0efc31128
Felix <javazorb@pixelinvaderz.com>
2025-04-02 17:12:51 +0200

    changed run to fit training routine

--- run.py
+++ run.py
@@ -1,16 +1,35 @@
 import json
 import os
 import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+import config
 import data.generate_environment as generate_data
 import data.dataset as data
 import tqdm
+import training.train_bc as train_bc
+import models.bc_model as bc_model
+import models.q_model as q_model
+import models.bcq_model as bcq_model
+from data.dataloader import EnvironmentDataset
 
 
 def run():
     #envs = data_gen()
+    #sets_generation()
+    behavior_cloning = bc_model.BehavioralModel()
+    train_data = data.load_dataset('train_data', 'data')
+    test_data = data.load_dataset('test_data', 'data')
+    val_data = data.load_dataset('val_data', 'data')
+    train_set = EnvironmentDataset(train_data)
+    val_set = EnvironmentDataset(val_data)
+    test_set = EnvironmentDataset(test_data)
+    train_bc.train(behavior_cloning, config.get_device(), train_set, val_set, nn.CrossEntropyLoss(), optim.Adam(behavior_cloning.parameters(), lr=0.001))
+
+
+def sets_generation():
     envs = generate_data.load_environments()
     optimal_paths = load_optimal_paths()
-    training_data, validation_data, testing_data = data.train_test_val_split(envs, optimal_paths)
     train_data, test_data, val_data = data.train_test_val_split(environments=envs, optimal_paths=optimal_paths)
     data.save_dataset(train_data, 'train_data')
     data.save_dataset(test_data, 'test_data')
