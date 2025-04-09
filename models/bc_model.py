9d78876729f33112784e646314904e7751ec6460
Felix <javazorb@pixelinvaderz.com>
2025-04-09 23:13:08 +0200

    changed model to use softmax for probability of max 1 per action

--- models/bc_model.py
+++ models/bc_model.py
@@ -7,7 +7,7 @@ class BehavioralModel(nn.Module):
         super().__init__(*args, **kwargs)
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(in_features=64 * 60 * 60, out_features=128)
+        self.fc1 = nn.Linear(in_features=64 * 60 * 5, out_features=128)
         self.fc2 = nn.Linear(in_features=128, out_features=len(config.Actions))
         self.relu = nn.ReLU()
 
@@ -17,4 +17,5 @@ class BehavioralModel(nn.Module):
         x = x.view(x.size(0), -1)
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
+        x = nn.functional.softmax(x, dim=1)
         return x
