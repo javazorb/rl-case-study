b80396518c76bd2ed055b3f21ab5116b1d3d5b03
Felix <javazorb@pixelinvaderz.com>
2025-12-21 17:27:16 +0100

    param tune

--- run.py
+++ run.py
@@ -63,8 +63,11 @@ def run():
     #acc, total_actions_predicted, num_counts_jump_right = train_bc.test_accuracy(behavior_cloning, config.get_device(), test_set)
 
     behavior_cloning = base_model.BaseModel()
+    train_bc_new.train_only_jump(behavior_cloning, config.get_device(), train_set, val_set,
+                                 optimizer=optim.AdamW(behavior_cloning.parameters(), lr=1e-4, weight_decay=1e-4),
+                                 criterion=None, early_stopping=3)
     train_bc_new.train(behavior_cloning, config.get_device(), train_set, val_set, optimizer=optim.AdamW(behavior_cloning.parameters(), lr=1e-4, weight_decay=1e-4), criterion=None)
-    train_bc_new.train_only_jump(behavior_cloning, config.get_device(), train_set, val_set, optimizer=optim.AdamW(behavior_cloning.parameters(), lr=1e-4, weight_decay=1e-4), criterion=None, early_stopping=3)
+
     behavior_cloning = load_model("final_BC_state_dict", behavior_cloning)
     acc, predicted_actions = train_bc_new.test_bc_accuracy(behavior_cloning, config.get_device(), val_set)
     #pred_array = np.array([p for batch in total_actions_predicted for p in batch])
