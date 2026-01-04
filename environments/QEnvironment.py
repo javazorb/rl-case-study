a340bf5a81889d2def2afa70d31072ec93161af1
Felix <javazorb@pixelinvaderz.com>
2026-01-04 19:08:49 +0100

    change, so that agent can't jump out of env

--- environments/QEnvironment.py
+++ environments/QEnvironment.py
@@ -273,7 +273,8 @@ class QEnvironment:
             reward -= 10
         if self.jump_count > 15:
             reward -= 5
-
+        if y > config.ENV_SIZE - 1:
+            y = config.ENV_SIZE - 1
         self.current_position = (x, y)
         if  (x == config.ENV_SIZE - 1 or self.current_position == self.goal_position
                 or self.current_position[0] == config.ENV_SIZE - 1):
