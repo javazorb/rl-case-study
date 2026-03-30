a2a9204c1796078d1dceb44b206b1a2793c6fd21
Felix <javazorb@pixelinvaderz.com>
2026-03-30 21:38:24 +0200

    fixed everything for crop

--- run.py
+++ run.py
@@ -137,7 +137,7 @@ def run():
                                                       optimal_paths=optimal_paths_both,
                                                       single=True)
 
-    # cropped_top_data_set = dataset = list(zip(cropped_top_data_set[0], cropped_top_data_set[1]))
+    cropped_top_data_set = dataset = list(zip(cropped_top_data_set[0], cropped_top_data_set[1]))
     run_experiment_1(agents, None, None, cropped_top_data_set, buffer, train=False,
                      experiment_name='cropped_environments_top')
     cropped_side_data_set = dataset = list(zip(cropped_side_data_set[0], cropped_side_data_set[1]))
